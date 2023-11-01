c73f6af63fc7d11524861489bbcd24873838dd65
elisaalbuquerque <elisa.silva81@etec.sp.gov.br>
2023-11-01 12:16:24 -0300

    vetor

--- vetor.c
+++ vetor.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int i;
-for (i=0;i<9;i=i*2){
-printf (
+int main() {
+	int vetor[10];
+	int i;
+
+	for (i = 0; i < 10; i++) {
+		printf("Digite um número: ");
+		scanf("%d", &vetor[i]);
+		vetor[i] *= 2; 
+	}
+
+	printf("Resultados:\n");
+	for (i = 0; i < 10; i++) {
+		printf("Posição %d: %d\n", i, vetor[i]);
+	}
+
+	return 0;
+}
+
